55d7f70749be75433398bb0f7563a6229887cedb
ieee <ieee@teicm.gr>
2017-11-30 17:41:50 +0200

    hello notifiers

--- app/src/main/java/com/example/makis/notificationstestproj/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/makis/notificationstestproj/MyFirebaseMessagingService.java
@@ -1,9 +1,13 @@
 package com.example.makis.notificationstestproj;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.media.SoundPool;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,19 +18,23 @@ import com.google.firebase.messaging.RemoteMessage;
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    static int id = 0;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Intent intent=new Intent(this,MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
-        NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(this);
-        notificationBuilder.setContentTitle("FCM notification");
+        NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(this,"1");
+        notificationBuilder.setContentTitle(remoteMessage.getNotification().getTitle());
         notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
+        notificationBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
         notificationBuilder.setAutoCancel(true);
         notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
         notificationBuilder.setContentIntent(pendingIntent);
 
         NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationBuilder.notify();
+        id++;
+        notificationManager.notify("app",id,notificationBuilder.build());
     }
 }
